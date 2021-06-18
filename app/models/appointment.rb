5dd4c42d8de97218857f14cfd751ac5019594ea1
helman101 <helmanandresortegon@gmail.com>
2021-06-18 15:59:00 +0200

    refactor Appoinment validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,5 +2,5 @@ class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :trainer
 
-  validate_presence_of :appointments_time, :status
+  validates_presence_of :appointment_time, :status
 end
