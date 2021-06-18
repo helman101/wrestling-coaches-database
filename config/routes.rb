bdc48fad24d4abf6ffc9f54ac8dc31e3c3efd32c
helman101 <helmanandresortegon@gmail.com>
2021-06-18 14:52:09 +0200

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :trainers, :users do
+  resources :trainers, only: [:index, :show, ] do
+    resources :appoinments
+  end
+
+  resources :users, only: [:create, :show, :destroy] do
     resources :appoinments
   end
 end
