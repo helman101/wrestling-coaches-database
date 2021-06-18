716eaa30eed5a57306741ed2ee6faf9880db8144
helman101 <helmanandresortegon@gmail.com>
2021-06-18 16:21:03 +0200

    create tests for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :appointments
+  has_many :appointments, dependent: :destroy
 
   validates_presence_of :name, :email
 end
--- spec/models/trainer_spec.rb
+++ spec/models/trainer_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Trainer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:appointments).dependent(:destroy) }
+  it { should validate_presence_of(:name)}
+  it { should validate_presence_of(:description)}
+  it { should validate_presence_of(:fighting_style)}
+  it { should validate_presence_of(:trainer_img)}
 end
