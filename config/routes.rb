e99f808d9f910ab59725d2498932193c18b54cc0
helman101 <helmanandresortegon@gmail.com>
2021-06-21 17:54:50 +0200

    delete appointments from trainers nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :trainers, only: [:index, :show] do
-    resources :appointments, only: [:index, :create, :destroy]
-  end
+  resources :trainers, only: [:index, :show]
 
   resources :users, only: [:create, :show, :destroy] do
     resources :appointments, only: [:index, :create, :destroy]
