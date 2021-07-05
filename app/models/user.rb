953a7435d5ae4d8cebcfc12550f3cf798f0c351e
helman101 <helmanandresortegon@gmail.com>
2021-07-05 15:37:45 +0200

    add email format validation and test validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :appointments, dependent: :destroy
-
   validates_presence_of :name, :email, :password, :password_confirmation
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates_uniqueness_of :name, :email
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
   it { should have_many(:appointments).dependent(:destroy) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
+  it { should allow_value("email@addresse.foo").for(:email) }
+  it { should_not allow_value("foo").for(:email) }
   it { should validate_presence_of(:password) }
   it { should validate_uniqueness_of(:name) }
   it { should validate_uniqueness_of(:email) }
